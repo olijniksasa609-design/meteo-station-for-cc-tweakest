3ad1603cc4fb3ea6a7dd070b95e68e2c464fd947
olijniksasa609-design <olijniksasa609@gmail.com>
2026-08-23 14:13:55 +0200

    Add files via upload

---
+++ meteo.lua
@@ -0,0 +1,38 @@
+local env = peripheral.wrap("left")
+local monitor = peripheral.wrap("right")
+
+if not env or not monitor then
+    print("Error: Connect Environment Detector and Monitor!")
+    return
+end
+
+monitor.setTextScale(1)
+monitor.clear()
+
+while true do
+    monitor.setCursorPos(1, 1)
+    monitor.write("=== METEO STATION ===")
+    
+    local time = env.getTime()
+    local isRaining = env.isRaining()
+    local isThundering = env.isThundering()
+    local dimension = env.getDimension()
+    
+    monitor.setCursorPos(1, 3)
+    monitor.write("Time: " .. tostring(time))
+    
+    monitor.setCursorPos(1, 4)
+    if isThundering then
+        monitor.write("Weather: Thunder ⚡")
+    elseif isRaining then
+        monitor.write("Weather: Rain 🌧️")
+    else
+        monitor.write("Weather: Clear ☀️")
+    end
+    
+    monitor.setCursorPos(1, 6)
+    monitor.write("Dim: " .. tostring(dimension))
+    
+    os.sleep(2)
+    monitor.clear()
+end
